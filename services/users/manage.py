628506045ddb638e0bd2b43a40642c6d9eddae3a
kkouts <kkoutsilis@gmail.com>
2021-03-11 15:43:35 +0200

    flask blueprint completed

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,11 +1,21 @@
 import sys
 import unittest
+
 from flask.cli import FlaskGroup
 
-from project import app, db  # new
+from project import create_app, db   # new
+from project.api.models import User  # new
+
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
 
-cli = FlaskGroup(app)
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 @cli.command()
 def test():
@@ -15,13 +25,6 @@ def test():
     if result.wasSuccessful():
         return 0
     sys.exit(result)
-
-# new
-@cli.command('recreate_db')
-def recreate_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 if __name__ == '__main__':
