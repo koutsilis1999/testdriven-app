ed13caa816c1ab36be6471c9b1701326ed5ba1b7
kkouts <kkoutsilis@gmail.com>
2021-03-11 14:52:57 +0200

    postgress setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,7 @@
+import os  # new
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
 
 
 # instantiate the app
@@ -8,7 +10,24 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
+
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
