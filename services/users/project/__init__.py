c94a029c96cfa0b14cfdfa944c28ae15e04928f4
kkouts <kkoutsilis@gmail.com>
2021-03-13 19:45:14 +0200

    added flask debug toolbar and fixed test_add_user_dublicate_email

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask  # new
+from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_debugtoolbar import DebugToolbarExtension
 
 # instantiate the db
 db = SQLAlchemy()
+
+toolbar = DebugToolbarExtension()
 
 
 # new
@@ -20,10 +22,10 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
-
     app.register_blueprint(users_blueprint)
 
     # shell context for flask cli
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,4 +1,4 @@
-import os  # new
+import os
 
 
 class BaseConfig:
@@ -6,23 +6,26 @@ class BaseConfig:
 
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "my_precious"  # new
+    SECRET_KEY = "my_precious"
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
 
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # new
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
