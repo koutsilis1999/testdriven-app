be26602d6997d1815dfab6607b2e093b4b8cec45
kkouts <kkoutsilis@gmail.com>
2021-03-19 22:34:47 +0200

    updated test.sh and .travis.yml

--- test.sh
+++ test.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
 
+
+type=$1
 fails=""
 
 inspect() {
-    if [ $1 -ne 0 ]; then
-        fails="${fails} $2"
-    fi  
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
 }
 
-# run unit and integration tests
-sudo docker-compose up -d --build
-sudo docker-compose exec users python manage.py test
-inspect $? users
-sudo docker-compose exec users flake8 project
-inspect $? users-lint
-sudo docker-compose exec client npm test -- --coverage
-inspect $? client
-sudo docker-compose down
+# run server-side tests
+server() {
+  sudo docker-compose up -d --build
+  sudo docker-compose exec users python manage.py test
+  inspect $? users
+  sudo docker-compose exec users flake8 project
+  inspect $? users-lint
+  sudo docker-compose down
+}
+
+# run client-side tests
+client() {
+  sudo docker-compose up -d --build
+  sudo docker-compose exec client npm test -- --coverage
+  inspect $? client
+  sudo docker-compose down
+}
 
 # run e2e tests
-sudo docker-compose -f docker-compose-prod.yml up -d --build
-sudo docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://localhost
-inspect $? e2e
-sudo docker-compose -f docker-compose-prod.yml down
+e2e() {
+  sudo docker-compose -f docker-compose-prod.yml up -d --build
+  sudo docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  inspect $? e2e
+  sudo docker-compose -f docker-compose-prod.yml down
+}
+
+# run all tests
+all() {
+  sudo docker-compose up -d --build
+  sudo docker-compose exec users python manage.py test
+  inspect $? users
+  sudo docker-compose exec users flake8 project
+  inspect $? users-lint
+  sudo docker-compose exec client npm test -- --coverage
+  inspect $? client
+  sudo docker-compose down
+  e2e
+}
+
+# run appropriate tests
+if [[ "${type}" == "server" ]]; then
+  echo "\n"
+  echo "Running server-side tests!\n"
+  server
+elif [[ "${type}" == "client" ]]; then
+  echo "\n"
+  echo "Running client-side tests!\n"
+  client
+elif [[ "${type}" == "e2e" ]]; then
+  echo "\n"
+  echo "Running e2e tests!\n"
+  e2e
+else
+  echo "\n"
+  echo "Running all tests!\n"
+  all
+fi
 
 # return proper code
 if [ -n "${fails}" ]; then
+  echo "\n"
   echo "Tests failed: ${fails}"
   exit 1
 else
+  echo "\n"
   echo "Tests passed!"
   exit 0
 fi
