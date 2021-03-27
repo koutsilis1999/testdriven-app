302e08d607d6c740ffd8776463351511ccffb693
kkouts <kkoutsilis@gmail.com>
2021-03-27 20:23:53 +0200

    added ace code editor

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,5 +1,13 @@
+const randomstring = require("randomstring");
+
+const username = randomstring.generate();
+const email = `${username}@test.com`;
+const password = "greaterthanten";
+
 it("should display the page correctly if a user is not logged in", () => {
   cy.visit("/")
+    .get("h1")
+    .contains("Exercises")
     .get(".navbar-burger")
     .click()
     .get("a")
@@ -12,5 +20,48 @@ it("should display the page correctly if a user is not logged in", () => {
     .contains("Log In")
     .get("a")
     .contains("Swagger")
+    .get("a")
+    .contains("Users")
+    .get(".notification.is-warning")
+    .contains("Please log in to submit an exercise.")
+    .not(".notification.is-success");
+});
+
+it("should display the page correctly if a user is logged in`", () => {
+  cy.server();
+  cy.route("POST", "auth/register").as("createUser");
+
+  // register user
+  cy.visit("/register")
+    .get('input[name="username"]')
+    .type(username)
+    .get('input[name="email"]')
+    .type(email)
+    .get('input[name="password"]')
+    .type(password)
+    .get('input[type="submit"]')
+    .click()
+    .wait("@createUser");
+
+  // assert '/' is displayed properly
+  cy.get("h1")
+    .contains("Exercises")
+    .get(".navbar-burger")
+    .click()
+    .get("a")
+    .contains("User Status")
+    .get("a")
+    .contains("Log Out")
+    .get("a")
+    .not("Register")
+    .get("a")
+    .not("Log In")
+    .get("a")
+    .contains("Swagger")
+    .get("a")
+    .contains("Users")
+    .get("button")
+    .contains("Run Code")
+    .not(".notification.is-warning")
     .not(".notification.is-success");
 });
