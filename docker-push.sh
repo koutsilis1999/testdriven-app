9c4b10f6274be58c355e178b4571d52742aea699
kkouts <kkoutsilis@gmail.com>
2021-03-28 18:20:29 +0300

    ecs deployment staging

--- docker-push.sh
+++ docker-push.sh
@@ -6,6 +6,7 @@ then
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-1752140886.us-west-1.elb.amazonaws.com"
+    export REACT_APP_EXERCISES_SERVICE_URL="http://testdriven-staging-alb-1752140886.us-west-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-1003607086.us-west-1.elb.amazonaws.com"
@@ -33,17 +34,25 @@ then
     docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
-    # # users db
-    # docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-    # docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-    # docker push $REPO/$USERS_DB:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL --build-arg REACT_APP_EXERCISES_SERVICE_URL=$REACT_APP_EXERCISES_SERVICE_URL --build-arg REACT_APP_API_GATEWAY_URL=$REACT_APP_API_GATEWAY_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
     docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
+    # exercises
+    docker build $EXERCISES_REPO -t $EXERCISES:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $EXERCISES:$COMMIT $REPO/$EXERCISES:$TAG
+    docker push $REPO/$EXERCISES:$TAG
+    # exercises db
+    docker build $EXERCISES_DB_REPO -t $EXERCISES_DB:$COMMIT -f Dockerfile
+    docker tag $EXERCISES_DB:$COMMIT $REPO/$EXERCISES_DB:$TAG
+    docker push $REPO/$EXERCISES_DB:$TAG
   fi
 fi
